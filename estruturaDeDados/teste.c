6ac1518c1a7b493e1937bf1dc289e85c6d6e5658
Galaxyvideok <lucasgarciasouza12@gmail.com>
2024-02-07 20:19:53 -0300

    bjbmnbm

--- estruturaDeDados/teste.c
+++ estruturaDeDados/teste.c
@@ -7,6 +7,18 @@
 #define CORTE "------------------------"
 
 int main(){
-    
+    int a, b, *p1, *p2;
+    a = 4;
+    b = 3;
+    p1 = &a;
+    p2 = p1;
+    *p2 = *p1 + 3;
+    b = b * (*p1);
+    (*p2)++;
+    p1 = &b;
+    printf("\n\n%s\n\n",RESULTADO);
+    printf("Valor: %d %d \n", *p1, *p2);
+    printf("Valor: %d %d \n", a, b);
+    printf("%s\n",CORTE);
     return 0;
 }
