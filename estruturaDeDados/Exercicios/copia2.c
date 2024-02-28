91267d159c753be039699fbb38ec8cba7a221a84
Galaxyvideok <lucasgarciasouza12@gmail.com>
2024-02-28 14:23:18 -0300

    ReformulandoAlgoritmo

--- estruturaDeDados/Exercicios/copia2.c
+++ estruturaDeDados/Exercicios/copia2.c
@@ -29,51 +29,7 @@ typedef struct tipoLista {
     int k;
 }Tlista;
 
-void digitarDados(TElemento *elementoNovo){
-    printf("Digite o nome da pessoa: ");
-    scanf(" %39[^\n]s", elementoNovo->nome);
-    printf("Digite a idade do individuo: ");
-    scanf("%f", &elementoNovo->idade);
-    printf("Digite a altura do individuo: ");
-    scanf("%f", &elementoNovo->altura);
-    printf("Digite o peso do individuo: ");
-    scanf("%f", &elementoNovo->peso);
-    elementoNovo->imc = (elementoNovo->peso/(elementoNovo->altura*elementoNovo->altura));
 
-}
-
-void pedirNome(char *nome){
-    printf("Digite o nome do individuo para excluir: ");
-    scanf(" %39[^\n]s", nome);
-}
-
-void inicializa(Tlista *lista){
-    lista->inicio = NULL;
-    lista->fim = NULL;
-    lista->total = 0;
-    printf("\n\n\tInforme quantidade de Grupos: ");
-    scanf("%d", &lista->k);
-    // inserir(lista,1.80,23,81,"Pedro");
-    // inserir(lista,1.76,21,67,"Anna");
-    // inserir(lista,1.72,21,61,"Cinthia");
-    // inserir(lista,1.72,17,72,"Jose");
-    // inserir(lista,1.77,35,96,"Silvano");
-    // inserir(lista,1.68,28,69,"Evelyn");
-    // inserir(lista,1.77,56,94,"Asdrubal");
-    // inserir(lista,1.75,35,85,"Jambira");
-    // inserir(lista,1.65,20,100,"Desiderio");
-    // inserir(lista,1.82,46,102,"Ramiro");
-    // inserir(lista,1.70,38,69,"Paula");
-    // inserir(lista,1.65,39,68,"Claudia");
-    // inserir(lista,1.66,18,65,"Sofia");
-    // inserir(lista,1.86,58,99,"Astolfo");
-    // inserir(lista,1.80,52,120,"Demostenes");
-    // inserir(lista,1.70,38,69,"Josefino");
-}
-
-void escolheCentroides(TLista *lista){
-   TElemento *atual = lista->inicio;
-}
 
 void digitarDados(TElemento *elementoNovo){
     printf("Digite o nome da pessoa: ");
@@ -99,23 +55,89 @@ void inicializa(Tlista *lista){
     lista->total = 0;
     printf("\n\n\tInforme quantidade de Grupos: ");
     scanf("%d", &lista->k);
-    // inserir(lista,1.80,23,81,"Pedro");
-    // inserir(lista,1.76,21,67,"Anna");
-    // inserir(lista,1.72,21,61,"Cinthia");
-    // inserir(lista,1.72,17,72,"Jose");
-    // inserir(lista,1.77,35,96,"Silvano");
-    // inserir(lista,1.68,28,69,"Evelyn");
-    // inserir(lista,1.77,56,94,"Asdrubal");
-    // inserir(lista,1.75,35,85,"Jambira");
-    // inserir(lista,1.65,20,100,"Desiderio");
-    // inserir(lista,1.82,46,102,"Ramiro");
-    // inserir(lista,1.70,38,69,"Paula");
-    // inserir(lista,1.65,39,68,"Claudia");
-    // inserir(lista,1.66,18,65,"Sofia");
-    // inserir(lista,1.86,58,99,"Astolfo");
-    // inserir(lista,1.80,52,120,"Demostenes");
-    // inserir(lista,1.70,38,69,"Josefino");
 }
+
+void escolheCentroides(Tlista *lista){
+    int j = 1;
+    do{
+        int centroideEscolhido = 0;
+        TElemento *atual = lista->inicio;
+        float escolhe = rand() % lista->total;
+        while (atual->prox != NULL){
+            if ((atual->altura <= escolhe)&&(centroideEscolhido != 1)){
+                atual->centroide = j;
+                centroideEscolhido = 1;
+            }
+            atual = atual->prox;
+        }
+    } while (j < lista->k);
+}
+
+void insere(Tlista *lista, float altura, float idade, float peso, char *nome){
+    int inseriu = 0;
+    TElemento *novo = (TElemento *)malloc(sizeof(TElemento));
+    novo->altura = altura;
+    novo->idade = idade;
+    novo->peso = peso;
+    strcpy(novo->nome,nome);
+    novo->prox = NULL;
+    novo->centroide = 0;
+    if (lista->inicio == NULL){
+        //Lista encontra-se vazia.
+        //Inserir o primeiro e unico elemento da lista ate agora
+        lista->inicio = novo;
+        lista->fim = novo;
+        lista->total = 1;
+        inseriu = 1;
+    }else{
+        //Lista ja possui pelo menos 1 elemento
+        TElemento *atual = lista->inicio;
+        TElemento *anterior = NULL;
+        while (atual != NULL){
+            if (strcmp(atual->nome,novo->nome) == 1){
+                if (atual == lista->inicio){
+                    //Inserir novo no inicio da lista
+                    novo->prox = atual;
+                    lista->inicio = novo;
+                }else{
+                    //Inserir novo no meio da lista
+                    novo->prox = atual;
+                    anterior->prox = novo;
+                }
+                inseriu = 1;
+                lista->total++;
+                break;
+            }
+            anterior = atual;
+            atual = atual->prox; //move para o próximo elemento
+        }
+        if (!inseriu){
+            //Inserir elemento no fim da lista
+            lista->fim->prox = novo;
+            lista->fim = novo;
+            lista->total++;
+        }
+        lista->total++;
+    }
+}
+
+// void digitarDados(TElemento *elementoNovo){
+//     printf("Digite o nome da pessoa: ");
+//     scanf(" %39[^\n]s", elementoNovo->nome);
+//     printf("Digite a idade do individuo: ");
+//     scanf("%f", &elementoNovo->idade);
+//     printf("Digite a altura do individuo: ");
+//     scanf("%f", &elementoNovo->altura);
+//     printf("Digite o peso do individuo: ");
+//     scanf("%f", &elementoNovo->peso);
+//     elementoNovo->imc = (elementoNovo->peso/(elementoNovo->altura*elementoNovo->altura));
+
+// }
+
+// void pedirNome(char *nome){
+//     printf("Digite o nome do individuo para excluir: ");
+//     scanf(" %39[^\n]s", nome);
+// }
 
 int pedirOpcao(){
     int op;
@@ -156,7 +178,7 @@ void inserir(Tlista *lista){
     TElemento *novo = (TElemento *)malloc(sizeof(TElemento));
     digitarDados(novo);
     novo->prox = NULL;
-    novo->centroide;
+    novo->centroide = 0;
     if (lista->inicio == NULL){
         //Lista encontra-se vazia.
         //Inserir o primeiro e unico elemento da lista ate agora
@@ -270,23 +292,25 @@ int pedirOpcao(){
     return op;
 }
 
-int pedirNum(){
-    int num;
-    do{
-        printf("Digite um numero para ser inserido: ");
-        scanf("%d", &num);
-    } while (num < 0);
-    return num;
-}
+// int pedirNum(){
+//     int num;
+//     do{
+//         printf("Digite um numero para ser inserido: ");
+//         scanf("%d", &num);
+//     } while (num < 0);
+//     return num;
+// }
 
-int pedirNum2(){
-    int num;
-    do{
-        printf("Digite um numero para ser excluido: ");
-        scanf("%d", &num);
-    } while (num < 0);
-    return num;
-}
+// int pedirNum2(){
+//     int num;
+//     do{
+//         printf("Digite um numero para ser excluido: ");
+//         scanf("%d", &num);
+//     } while (num < 0);
+//     return num;
+// }
+
+//Usa isrand()
 
 Tlista lista; //variavel global
 
@@ -303,7 +327,23 @@ int main(){
         switch (op){
         case 1:
             // numInseri = pedirNum();
-            inserir(&lista);
+            // inserir(&lista);
+            insere(&lista,1.80,23,81,"Pedro");
+            insere(&lista,1.76,21,67,"Anna");
+            insere(&lista,1.72,21,61,"Cinthia");
+            insere(&lista,1.72,17,72,"Jose");
+            insere(&lista,1.77,35,96,"Silvano");
+            insere(&lista,1.68,28,69,"Evelyn");
+            insere(&lista,1.77,56,94,"Asdrubal");
+            insere(&lista,1.75,35,85,"Jambira");
+            insere(&lista,1.65,20,100,"Desiderio");
+            insere(&lista,1.82,46,102,"Ramiro");
+            insere(&lista,1.70,38,69,"Paula");
+            insere(&lista,1.65,39,68,"Claudia");
+            insere(&lista,1.66,18,65,"Sofia");
+            insere(&lista,1.86,58,99,"Astolfo");
+            insere(&lista,1.80,52,120,"Demostenes");
+            insere(&lista,1.70,38,69,"Josefino");
             break;
         case 2:
             exibeLista(lista);
