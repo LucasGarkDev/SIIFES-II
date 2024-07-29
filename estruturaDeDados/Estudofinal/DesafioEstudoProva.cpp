d042ecd8f104f7be981da8dc3e3cfb3c882b106a
Lucas <lucasgarciasouza12@gmail.com>
2024-07-29 17:19:13 -0300

    dc

---
+++ estruturaDeDados/Estudofinal/DesafioEstudoProva.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Inclua os arquivos que contêm as implementações das funções aqui
+// #include "lista_encadeada.h"
+// #include "pilha.h"
+// #include "avl.h"
+
+int main() {
+    // Testando a Lista Encadeada
+    // TNoLista *lista = NULL;
+    // inserirLista(&lista, 5);
+    // inserirLista(&lista, 3);
+    // inserirLista(&lista, 7);
+    // inserirLista(&lista, 1);
+    // printf("Lista encadeada: ");
+    // exibirLista(lista);
+    // excluirLista(&lista, 3);
+    // printf("Lista encadeada após exclusão de 3: ");
+    // exibirLista(lista);
+    // printf("Busca 7 na lista: %d\n", buscarLista(lista, 7));
+
+    // // Testando a Pilha
+    // TPilha pilha;
+    // inicializaPilha(&pilha);
+    // empilha(&pilha, 10);
+    // empilha(&pilha, 20);
+    // empilha(&pilha, 30);
+    // printf("Pilha: ");
+    // exibirPilha(&pilha);
+    // desempilha(&pilha);
+    // printf("Pilha após desempilhar: ");
+    // exibirPilha(&pilha);
+    
+    // // Testando a Árvore AVL
+    // TNoAVL *raiz = NULL;
+    // raiz = insere(raiz, 10);
+    // raiz = insere(raiz, 20);
+    // raiz = insere(raiz, 5);
+    // raiz = insere(raiz, 6);
+    // raiz = insere(raiz, 8);
+    // printf("Árvore AVL em ordem: ");
+    // caminhamentoEmOrdem(raiz);
+    // printf("\nBusca 20 na árvore AVL: %d\n", busca(raiz, 20));
+
+    return 0;
+}
+
