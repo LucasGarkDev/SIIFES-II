bfc66573e6398bc4ffba3b6b630e7367a134a266
Lucas <lucasgarciasouza12@gmail.com>
2024-08-15 13:33:46 -0300

    ffdd

--- TPA/aula1.c
+++ TPA/aula1.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    int *p;
-    *p = 516;
-    *p = 32;
+// int main(){
+//     int *p;
+//     p = 516;
+//     *p = 32;
 
-    printf(p);
+//     printf("%d",*p);
+// }
+
+int main(){
+    int *p = (int*)malloc(sizeof(int));
+    int *q;
+    *p = 516;
+    q = p;
+    *q = 32;
+    printf("%d %d\n",*p,*q);
+    // 32 32
 }
+
+
 
