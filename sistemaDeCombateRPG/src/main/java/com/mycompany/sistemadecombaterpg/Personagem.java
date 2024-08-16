2d550c791eaeb1e55727ccea2294e792f489163b
Lucas <lucasgarciasouza12@gmail.com>
2024-08-15 21:43:05 -0300

    gjgjgj

---
+++ sistemaDeCombateRPG/src/main/java/com/mycompany/sistemadecombaterpg/Personagem.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistemadecombaterpg;
+
+/**
+ *
+ * @author lucas
+ */
+public class Personagem {
+    String nome;
+    int forca;
+    int destresa;
+    int constituicao;
+    int inteligencia;
+    int sabedoria;
+    int carisma;
+    int classeDeArmadura;
+    int pontosDeVida;
+    int pontosDeVidaMaximo;
+    int nivel;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getForca() {
+        return forca;
+    }
+    public void setForca(int forca) {
+        this.forca = forca;
+    }
+    public int getDestresa() {
+        return destresa;
+    }
+    public void setDestresa(int destresa) {
+        this.destresa = destresa;
+    }
+    public int getConstituicao() {
+        return constituicao;
+    }
+    public void setConstituicao(int constituicao) {
+        this.constituicao = constituicao;
+    }
+    public int getInteligencia() {
+        return inteligencia;
+    }
+    public void setInteligencia(int inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+    public int getSabedoria() {
+        return sabedoria;
+    }
+    public void setSabedoria(int sabedoria) {
+        this.sabedoria = sabedoria;
+    }
+    public int getCarisma() {
+        return carisma;
+    }
+    public void setCarisma(int carisma) {
+        this.carisma = carisma;
+    }
+    public int getClasseDeArmadura() {
+        return classeDeArmadura;
+    }
+    public void setClasseDeArmadura(int classeDeArmadura) {
+        this.classeDeArmadura = classeDeArmadura;
+    }
+    public int getPontosDeVida() {
+        return pontosDeVida;
+    }
+    public void setPontosDeVida(int pontosDeVida) {
+        this.pontosDeVida = pontosDeVida;
+    }
+    public int getNivel() {
+        return nivel;
+    }
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+}
+
+
+
+
