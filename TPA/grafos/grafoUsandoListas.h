cdaeee72306f6ffc7a2aca6804827c65c9dfb455
Lucas <lucasgarciasouza12@gmail.com>
2024-10-17 13:32:19 -0300

    dfdf

---
+++ TPA/grafos/grafoUsandoListas.h
@@ -0,0 +1,18 @@
+//Feito por: Lucas Garcia E Luis Augusto
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <time.h>
+
+typedef char string[101];
+typedef clock_t processTime;
+
+typedef struct tipoCidade{
+    string nome;
+    float pesoAresta;
+}Cidade;
+
+
+
